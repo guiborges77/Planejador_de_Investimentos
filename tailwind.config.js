4c443554ca1fa4f62a5b4b56342c26ddbf4d4eb8
guiborges77 <guironaldao@gmail.com>
2024-12-16 21:11:29 -0300

    Corrigir Tailwind config para ES Modules

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+import defaultTheme from "tailwindcss/defaultTheme";
+import tailwindcssAnimate from "tailwindcss-animate";
 
+/** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -40,5 +42,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 };
